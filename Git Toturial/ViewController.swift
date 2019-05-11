c8805029abbb8530a8c436fcb1159abe886d0618
PangYen Chen <cpy4869@gmail.com>
2019-05-11 12:24:00 +0800

    第一次commit

--- Git Toturial/ViewController.swift
+++ Git Toturial/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let str = "Hello world"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(str)
     }
 
 
